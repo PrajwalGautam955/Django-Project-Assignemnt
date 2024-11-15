7ccef0c5d50738611152b01467094da58b9b05b1
Prajwal Gautam <141403909+PrajwalGautam955@users.noreply.github.com>
2024-11-15 12:37:16 +0530

    Update settings.py

--- Django Project/Django Project/DevTest/DevTest/settings.py
+++ Django Project/Django Project/DevTest/DevTest/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+ALLOWED_HOSTS = ['13.48.42.52', 'api.ipify.org']
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
